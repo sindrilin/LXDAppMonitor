e2ab702e53f5e511ec44b42efe63f4eec30daa4f
林欣达 <634893985@qq.com>
2018-05-30 21:50:36 +0800

    Update main.m

--- LXDAppMonitor/main.m
+++ LXDAppMonitor/main.m
@@ -16,21 +16,3 @@ int main(int argc, char * argv[]) {
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
-
-- (void)startMonitoring {
-    NSArray *_lastStackFrames;
-    CGFloat _pingInterval = 3;
-    dispatch_source_t timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, self.fpsMonitorQueue);
-    dispatch_source_set_timer(timer, DISPATCH_TIME_NOW, _pingInterval * NSEC_PER_SEC, 0 * NSEC_PER_SEC);
-    
-    dispatch_source_set_event_handler(timer, ^{
-        NSArray *curStackFrames = [XXStackFrameBacktracer backtraceMainThreadStackFrames];
-        if ([_lastStackFrames isEqual: curStackFrames]) {
-            [XXStackFramesUploader uploadStackFrames: curStackFrames completion: nil];
-            _lastStackFrames = nil;
-        } else {
-            _lastStackFrames = curStackFrames;
-        }
-    });
-    dispatch_resume(timer);
-}
